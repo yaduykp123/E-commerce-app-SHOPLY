ea2e6d45d4bedbedd1af7a53a78748d28a282f97
Yadu Krishnan <yaduykp123@gmail.com>
2026-06-18 10:18:29 +0530

    changed .env

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { StoreProvider } from './CONTEXT/StoreContext.jsx'
 import { AdminProductProvider } from './Admin/context/ProductContext.jsx'
 import axios from 'axios';
 
-axios.defaults.baseURL = `${import.meta.env.VITE_API_URL || 'http://localhost:5000'}/api`;
+axios.defaults.baseURL = `${import.meta.env.VITE_API_URL}/api`;
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
